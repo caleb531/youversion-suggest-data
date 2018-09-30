ab3ed830584641041a10bc5cde12ea88ae703823
Caleb Evans <caleb@calebevans.me>
2018-09-30 14:49:44 -0700

    Update language parser to use YouVersion JSON API

--- tests/test_get_language_name.py
+++ tests/test_get_language_name.py
@@ -8,13 +8,13 @@ import requests
 from mock import NonCallableMock, patch
 
 import tests
-from utilities.language_parser import get_language_name, get_languages_html
+from utilities.language_parser import get_language_name, get_languages_json
 
-with open('tests/html/languages.html') as html_file:
-    html_content = html_file.read().decode('utf-8')
+with open('tests/json/languages.json') as json_file:
+    json_content = json_file.read().decode('utf-8')
     patch_requests_get = patch(
         'requests.get', return_value=NonCallableMock(
-            text=html_content))
+            text=json_content))
 
 
 def set_up():
@@ -31,18 +31,18 @@ def tear_down():
 def test_get_language_name():
     """should fetch language name for the given language ID"""
     language_name = get_language_name('spa_es')
-    nose.assert_equal(language_name, 'Español (España) - Spanish (Spain)')
+    nose.assert_equal(language_name, 'Español (España)')
 
 
 @nose.with_setup(set_up, tear_down)
 def test_get_language_name_cache():
     """should cache languages HTML after initial fetch"""
-    if hasattr(get_languages_html, 'cache_clear'):
-        get_languages_html.cache_clear()
+    if hasattr(get_languages_json, 'cache_clear'):
+        get_languages_json.cache_clear()
     get_language_name('spa')
     language_name = get_language_name('fra')
     requests.get.assert_called_once()
-    nose.assert_equal(language_name, 'Français - French')
+    nose.assert_equal(language_name, 'Français')
 
 
 @nose.with_setup(set_up, tear_down)
--- utilities/language_parser.py
+++ utilities/language_parser.py
@@ -3,25 +3,37 @@
 
 from __future__ import unicode_literals
 
+import json
+
 import cachetools.func
 import requests
-from pyquery import PyQuery as pq
 
 
-# fetch to speed up subsequent requests
 @cachetools.func.lru_cache(maxsize=1)
-def get_languages_html():
-    return requests.get('https://www.bible.com/languages').text
+def get_languages_json():
+    return json.loads(
+        requests.get('https://www.bible.com/json/bible/languages').text)
+
+
+def get_language(raw_language):
+    return {
+        'id': raw_language['tag'],
+        'name': raw_language['local_name']
+    }
 
 
 # Retrieves the language with
 def get_language_name(language_id):
 
-    d = pq(get_languages_html())
-    lang_elem = d('a[href="/languages/{}"]'.format(language_id))
+    raw_languages = get_languages_json()
 
-    language_name = lang_elem.text()
-    if not language_name:
+    languages = [get_language(raw_language)
+                 for raw_language in raw_languages['items']]
+    if not languages:
         raise RuntimeError('Cannot retrieve language data')
 
-    return language_name
+    for language in languages:
+        if language['id'] == language_id:
+            return language['name']
+
+    raise RuntimeError('Cannot find language with name')
