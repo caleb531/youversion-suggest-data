854b8832e2c7db181eaf81b52716a28cf745cb91
Caleb Evans <caleb@calebevans.me>
2018-09-30 16:50:36 -0700

    Bring language parser coverage to 100%

--- tests/test_get_language_name.py
+++ tests/test_get_language_name.py
@@ -25,6 +25,7 @@ def set_up():
 def tear_down():
     patch_requests_get.stop()
     tests.tear_down()
+    get_languages_json.cache_clear()
 
 
 @nose.with_setup(set_up, tear_down)
@@ -43,6 +44,22 @@ def test_get_language_name_cache():
     language_name = get_language_name('fra')
     requests.get.assert_called_once()
     nose.assert_equal(language_name, 'Français')
+
+
+@nose.with_setup(set_up, tear_down)
+@patch('requests.get', return_value=NonCallableMock(text='{}'))
+def test_get_language_name_no_data(requests_get):
+    """should raise error when language list cannot be found"""
+    with nose.assert_raises(RuntimeError):
+        get_language_name(language_id='eng')
+
+
+@nose.with_setup(set_up, tear_down)
+@patch('requests.get', return_value=NonCallableMock(text='{"items":[]}'))
+def test_get_language_name_empty(requests_get):
+    """should raise error when language list is empty"""
+    with nose.assert_raises(RuntimeError):
+        get_language_name(language_id='eng')
 
 
 @nose.with_setup(set_up, tear_down)
