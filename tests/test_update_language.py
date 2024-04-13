c872feb6ab338672b8e23f8636fe7d6cf7402e9f
Caleb Evans <caleb@calebevans.me>
2024-04-13 10:58:12 -0700

    Add missing test for changing default version on language update

--- tests/test_update_language.py
+++ tests/test_update_language.py
@@ -33,6 +33,14 @@ class TestUpdateLanguage(YVSTestCase):
         update_lang.main()
         add_language.assert_called_once_with(language_id="swe", default_version=154)
 
+    @patch("sys.argv", [update_lang.__file__, "eng", "--default-version", "116"])
+    @patch("utilities.update_language.add_language")
+    @redirect_stdout
+    def test_main_change_default_version(self, out, add_language):
+        """main function should pass correct arguments to add_language"""
+        update_lang.main()
+        add_language.assert_called_once_with(language_id="eng", default_version=116)
+
     @patch("utilities.update_language.update_language", side_effect=KeyboardInterrupt)
     @patch("utilities.update_language.parse_cli_args")
     @redirect_stdout
